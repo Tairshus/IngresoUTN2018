aae1c036810938b60c901a2fffe309c909af54cd
Tairshus <36230542+Tairshus@users.noreply.github.com>
2018-02-21 12:31:28 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,14 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var flag=0;
 	var positivo=0;
 	var negativo=1;
 	var numero;
 	
 	var respuesta='si';
 	
-	while (respuesta == "si")
+	do
 	{
 		numero = parseInt(prompt("Ingrese un numero"));
 		if (numero >=0)
@@ -18,13 +18,18 @@ function Mostrar()
 		else
 		{
 			negativo = negativo * numero;
+			flag++
 		}
 		respuesta = prompt("Para continuar ingresando numeros escriba si");
 		
-	} 
+	} while (respuesta == "si")
 
 
 document.getElementById('suma').value=positivo;
+if (flag == 0)
+{
+	negativo = 0;
+}
 document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,16 +1,27 @@
 function Mostrar()
 {
-
-	var contador=0;
+    var contador;
 	// declarar variables
-	
-	var respuesta='si';
+	var numeroMayor;
+    var numeroMenor;
+	var respuesta="si";
+	var numeroNuevo;
 
-	while(respuesta!='no')
+	while(respuesta!="no")
 	{
+		numeroNuevo = parseInt(prompt("Ingrese un numero"));
+
+		contador = [0 , numeroNuevo];
+
+		numeroMayor = Math.max(contador);
+		numeroMenor = Math.min(contador);
 		
+		respuesta = prompt("¿Cotinuar ingresando numeros?");
 	
 	}
+
+	document.getElementById("maximo").value = numeroMayor;
+	document.getElementById("minimo").value = numeroMenor;
 
 
 
